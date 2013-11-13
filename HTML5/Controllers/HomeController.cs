bbafe3e250391784152ff18e59a3be7d08228831
Joemen M Bayan <joemenbayan@gmail.com>
2013-11-13 21:16:00 +0800

    Updates: HTML5

---
+++ HTML5/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HTML5.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SaveCanvas(string data)
+        {
+            Session["canvas_data"] = data;
+            return Json("Canvas data stored successfully!");
+        }
+
+    }
+}
